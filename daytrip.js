7080e2f832ac2f95a213793b4fa04c0fcf110887
Al Green <a.l.green@hotmail.com>
2021-10-05 21:51:26 -0500

    Updated for functions in daytrip.js, added random to select index

---
+++ daytrip.js
@@ -0,0 +1,42 @@
+userInput = prompt('Select a Destination');
+
+let places = ['HI','TX','CA','NY','FL'];
+let rstrnt = ['POKE','BBQ','STEAK','SUSHI','RAMEN'];
+let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
+let transpo = ['CAR','LIMO','BUS','PLANE','HELO'];
+
+function dayTripLoc(dest){
+
+   let d=  Math.floor(Math.random() * 4) +1);
+
+   console.log(dest[d]);  
+        
+        
+    
+    
+}
+
+// function dayTripRes(food){
+//     for (let f = 0; f < food.length; f++) {
+       
+        
+//     }
+    
+// }
+
+// function dayTripTrans(whip){
+//     for (let w = 0; w < whip.length; w++) {
+       
+        
+//     }
+    
+// }
+
+// function dayTripEnt(fun){
+//     for (let f = 0; f < fun.length; f++) {
+        
+        
+//     }
+    
+// }
+
