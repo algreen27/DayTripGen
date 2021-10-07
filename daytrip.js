7f2aa36f02fa59e979f83c648c3e19e7527722ae
Al Green <a.l.green@hotmail.com>
2021-10-06 19:43:37 -0500

    Update 1st function call to choose location based on random number selection

--- daytrip.js
+++ daytrip.js
@@ -1,24 +1,42 @@
 //userInput = prompt('Select a Destination');
 
+
 let places = ['HI','TX','CA','NY','FL'];
-let rstrnt = ['POKE','BBQ','STEAK','SUSHI','RAMEN'];
-let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
-let transpo = ['CAR','LIMO','BUS','PLANE','HELO'];
-
+//let Y = true;
+//let N = false;
+let opt1 = dayTripLoc;
 function dayTripLoc(dest){
+//    for (let d = 0; d < dest.length; d++) {
+        let d = (Math.floor(Math.random() * 4));     
+        let userInput = prompt(`Would you like to go to ${dest[d]}? Y or N`);
+            if(userInput == Y){
+               dayTripRes.apply(food);
+                console.log(`You are headed to ${dest[d]}`);
+            }
+            else if(userInput == N){
+                let opt1 = dayTripLoc;
+            }   
 
-   let d =  Math.floor(Math.random() * 4);
-   //capture number in d
-    
-    
-    
+
 }
 
 dayTripLoc(places);
-console.log(places[d]); 
+ 
+let rstrnt = ['POKE','BBQ','STEAK','SUSHI','RAMEN'];
 
-// function dayTripRes(food){
-//     for (let f = 0; f < food.length; f++) {
+function dayTripRes(food){
+    let f = (Math.floor(Math.random() * 4));     
+    userInput = prompt(`Would you like ${food[f]} to eat? Y or N`);
+        if(userInput === Y){
+            dayTripRes;
+            console.log(`You will be eating ${food}`)
+        }
+        else if(userInput === N){
+            dayTripLoc;
+        }
+}
+//     for (let f = 0; f < food.length; f++) {}
+
        
         
 //     }
@@ -26,6 +44,7 @@ console.log(places[d]);
 // }
 
 // function dayTripTrans(whip){
+ //   let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
 //     for (let w = 0; w < whip.length; w++) {
        
         
@@ -34,10 +53,10 @@ console.log(places[d]);
 // }
 
 // function dayTripEnt(fun){
+    //let transpo = ['CAR','LIMO','BUS','PLANE','HELO'];
 //     for (let f = 0; f < fun.length; f++) {
         
         
 //     }
     
-// }
-
+// 
