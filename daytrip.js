83ecdd9a649686cae867581cdd6bc5ea384057ef
Al Green <a.l.green@hotmail.com>
2021-10-07 22:28:48 -0500

    added functionality to randomly select destination, restaurant, and transportation. Unable to get console.log and result to function properly

--- daytrip.js
+++ daytrip.js
@@ -1,33 +1,31 @@
-//userInput = prompt('Select a Destination');
-
 
 let places = ['HI','TX','CA','NY','FL'];
 
-//let Y = true;
-//let N = false;
-//let opt1 = dayTripLoc;
 
 function dayTripLoc(dest){
 //    for (let d = 0; d < dest.length; d++) {
         let d = (Math.floor(Math.random() * 4));     
-        let userInput = prompt(`Would you like to go to ${dest[d]}? Y or N`);
-            if(userInput == 'Y'){
-                
-            //try to call next function dayTripRes;
-             //   console.log(`You are headed to ${dest[d]}`);
-            }
-            else if(userInput == 'N'){
-                let opt1 = dayTripLoc;
-            }   
-            let result = `you're headed to ${dest[d]}`;
-            return result;
-            console.log(result);
+        let userInput = prompt(`Would you like to go to ${dest[d]}? 1 for Y ,2 for N`);
+        for (let index = 0; index < 2; index++) {
+            if(userInput === 1){
+                //     let result = `you're headed to ${dest[d]}`;
+               console.log(`you're headed to ${dest[d]}`);
+                  //   return result;
+                     
+                 }   
+            else if(userInput === N){
+                dayTripLoc;
+
+               
+               
+        }
+            
 
 }
 
 dayTripLoc(places);
  
-// let rstrnt = ['POKE','BBQ','STEAK','SUSHI','RAMEN'];
+let rstrnt = ['POKE','BBQ','STEAK','SUSHI','RAMEN'];
 
 function dayTripRes(food){
     let f = (Math.floor(Math.random() * 4));     
@@ -42,40 +40,44 @@ function dayTripRes(food){
 }
 
 dayTripRes(rstrnt);
-// //     for (let f = 0; f < food.length; f++) {}
-
-       
-        
-// //     }
-    
-// let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
-
-// function dayTripEnt(entr){
-//     let e = (Math.floor(Math.random() * 4));     
-//     userInput = prompt(`Would you like ${entr[e]} to eat? Y or N`);
-//         if(userInput === Y){
-//             dayTripTrans;
-//             console.log(`You will go to the ${entr} for a good time!`)
-//         }
-//         else if(userInput === N){
-//             dayTripLoc;
-//         }
-//  }   //let transpo = ['CAR','LIMO','BUS','PLANE','HELO'];
-// //     for (let f = 0; f < fun.length; f++) {
 
 
 
-// // function dayTripTrans(whip){
-//  //   let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
-// //     for (let w = 0; w < whip.length; w++) {
-       
-        
-// //     }
-    
-// // }
+let entrtn = ['BAR','CLUB','THEATER','MOVIE','SURF','RODEO'];
 
-        
-        
-// //     }
-    
-// // 
+function dayTripEnt(entr){
+    let e = (Math.floor(Math.random() * 4));     
+    userInput = prompt(`Would you like ${entr[e]} to eat? Y or N`);
+        if(userInput === Y){
+            dayTripTrans;
+            console.log(`You will go to the ${entr} for a good time!`)
+        }
+        else if(userInput === N){
+            dayTripLoc;
+        }
+}   
+
+
+dayTripEnt(entrtn);
+
+
+
+let transpo = ['CAR','LIMO','BUS','PLANE','HELO'];
+
+
+function dayTripTrans(whip){
+    let f = (Math.floor(Math.random() * 4));     
+    userInput = prompt(`Would you like ${food[f]} to eat? Y or N`);
+        if(userInput === Y){
+            dayTripRes;
+            console.log(`You will be eating ${food}`)
+        }
+        else if(userInput === N){
+            dayTripLoc;
+        }
+}
+
+dayTripTrans(transpo);
+
+
+
